3626b58af3f28ce14510d7f74741fcc7e5ec2479
AFFONSO DAVI DA SILVA <affonso_silva@estudante.sesisenai.org.br>
2023-10-17 21:52:21 -0300

    OT-01 NodeJS

---
+++ Projeto_Hello_world/hello_world.js
@@ -0,0 +1,1 @@
+console.log("Olá mundo !! Em Node.js");
---
+++ calculadoraNode/calc.js
@@ -0,0 +1,6 @@
+let SomaFunc = require("./somar");
+
+console.log(`O valor da soma é: ${SomaFunc.soma(3,2)}`);
+console.log(`O valor da divisão é: ${SomaFunc.divisao(6,2)}`);
+console.log(`O valor da subtração é: ${SomaFunc.subtracao(8,5)}`);
+console.log(`O valor da multiplicação é: ${SomaFunc.multiplicacao(4,7)}`);
